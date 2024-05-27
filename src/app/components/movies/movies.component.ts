e37a16d14742755e8eee8bc734225701898d7738
nitesh <pandeynitesh822@gmail.com>
2024-05-27 09:45:12 +0530

    make Api call successfully

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { ServicesService } from '../services.service';
 interface Slide {
-  url: string;
+  id: number;
   title: string;
+  overview: string;
+  posterPath: string;
+  runtime: string;
+  genres: string;
 }
 @Component({
   selector: 'app-movies',
@@ -9,53 +14,72 @@ interface Slide {
   styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent {
-  slides: Slide[] = [
-    {
-      url: '../../../assets/Images/arrival.jpg',
-      title: 'Arrival',
-    },
-    {
-      url: '../../../assets/Images/blade runner.jpg',
-      title: 'Blade Runner',
-    },
-    {
-      url: '../../../assets/Images/DuneImage.jpg',
-      title: 'Dune',
-    },
-    {
-      url: '../../../assets/Images/star war.png',
-      title: 'Star War',
-    },
-    {
-      url: '../../../assets/Images/The tomorrow war.jpg',
-      title: 'The tomorrow war',
-    },
-    {
-      url: '../../../assets/Images/DuneImage.jpg',
-      title: 'Dune',
-    },
-    {
-      url: '../../../assets/Images/star war.png',
-      title: 'Star War',
-    },
-    {
-      url: '../../../assets/Images/The tomorrow war.jpg',
-      title: 'The tomorrow war',
-    },
-    {
-      url: 'https://via.placeholder.com/300x400?text=Movie+6',
-      title: 'Movie 6',
-    },
-    {
-      url: 'https://via.placeholder.com/300x400?text=Movie+7',
-      title: 'Movie 7',
-    },
-  ];
+  constructor(private movieService: ServicesService) {}
+  slides: Slide[] = [];
+  // slides: Slide[] = [
+  //   {
+  //     url: '../../../assets/Images/arrival.jpg',
+  //     title: 'Arrival',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/blade runner.jpg',
+  //     title: 'Blade Runner',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/DuneImage.jpg',
+  //     title: 'Dune',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/star war.png',
+  //     title: 'Star War',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/The tomorrow war.jpg',
+  //     title: 'The tomorrow war',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/DuneImage.jpg',
+  //     title: 'Dune',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/star war.png',
+  //     title: 'Star War',
+  //   },
+  //   {
+  //     url: '../../../assets/Images/The tomorrow war.jpg',
+  //     title: 'The tomorrow war',
+  //   },
+  //   {
+  //     url: 'https://via.placeholder.com/300x400?text=Movie+6',
+  //     title: 'Movie 6',
+  //   },
+  //   {
+  //     url: 'https://via.placeholder.com/300x400?text=Movie+7',
+  //     title: 'Movie 7',
+  //   },
+  // ];
   currentIndex = 0;
   translateX = 0;
   intervalId: any;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.movieService.getMovies().subscribe((res) => {
+      res.results.map((movie: any) => {
+        this.slides.push({
+          id: movie.id,
+          title: movie.title,
+          overview: movie.overview,
+          posterPath: movie.poster_path,
+          runtime: movie.details?.runtime,
+          genres: movie.details?.genres,
+        });
+      });
+    });
+  }
+
+  getFullImageUrl(posterPath: string): string {
+    return `https://image.tmdb.org/t/p/original${posterPath}`;
+  }
 
   ngOnDestroy() {}
 
--- src/app/components/services.service.ts
+++ src/app/components/services.service.ts
@@ -1,9 +1,24 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Params } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServicesService {
   constructor(private http: HttpClient) {}
+
+  getMovies() {
+    const httpHeaders: HttpHeaders = new HttpHeaders({
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2Y5OTFmNzA0ZGIwZGI1ZTA4ZjJlNTAzNjkwYmYyOCIsInN1YiI6IjYyYTc1MzU5N2Y2YzhkNzdiYmE0OTVjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UoY4H_XbQLNIhC0MJWR6wQ9C0Qiwc9DLCEQyIevE8RU',
+    });
+    return this.http.get<any>(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=a7f991f704db0db5e08f2e503690bf28',
+      {
+        headers: httpHeaders,
+      }
+    );
+  }
 }
